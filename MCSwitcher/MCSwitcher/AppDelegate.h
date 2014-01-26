1041e85e17097c9dc01b120096f6e6941f09d809
CasperCL <casper@labela.nl>
2014-01-26 22:17:02 +0100

    Initial Commit

---
+++ MCSwitcher/MCSwitcher/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MCSwitcher
+//
+//  Created by Casper Eekhof on 26-01-14.
+//  Copyright (c) 2014 Redlake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MCSwitcher/MCSwitcher/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MCSwitcher
+//
+//  Created by Casper Eekhof on 26-01-14.
+//  Copyright (c) 2014 Redlake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
