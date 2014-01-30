395a7aa10bbf269faf930c6b2a941ad3ede5515d
CasperCL <casperjeekhof@gmail.com>
2014-01-30 18:55:08 +0100

    Now loads and displays levels. Also allows you to edit them

--- MCSwitcher/Classes/View/WorldsViewController.m
+++ MCSwitcher/Classes/View/WorldsViewController.m
@@ -8,8 +8,10 @@
 
 #import "WorldsViewController.h"
 #import "MCLevelInfoCell.h"
+#import "LevelsController.h"
 
 @interface WorldsViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *levelsTableView;
 
 @end
 
@@ -19,12 +21,27 @@
 {
     [super viewDidLoad];
     
+    NSError * error;
+    _levels = [[LevelsController shared] loadLevels:&error];
     
+    [_levelsTableView reloadData];
 }
 
 #pragma mark - UITableViewDelegate
--(void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath {
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    MCLevelInfoCell * levelCell = (MCLevelInfoCell*)[tableView cellForRowAtIndexPath: indexPath];
     
+    NSError * err;
+    Level*level = [_levels objectAtIndex:indexPath.row];
+    [[LevelsController shared] toggleMode:level error: &err];
+    
+    if(!err) {
+        if([level gameType] == GameType_Creative) {
+            [[levelCell thumbnailImage] setImage:[UIImage imageNamed:@"pickaxe"]];
+        } else {
+            [[levelCell thumbnailImage] setImage:[UIImage imageNamed:@"survival"]];
+        }
+    }
 }
 
 #pragma mark - UITableViewDataSource
@@ -42,8 +59,23 @@
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     MCLevelInfoCell * levelCell = [tableView dequeueReusableCellWithIdentifier:@"MCLevelInfoCell" forIndexPath: indexPath];
+    [levelCell setSelectionStyle: UITableViewCellSelectionStyleNone];
+    Level * level = [_levels objectAtIndex: indexPath.row];
+    [[levelCell titleLabel] setText: [level levelName]];
+    [[levelCell titleLabel] setFont:font_kc_med(16)];
     
-
+    NSDate * lastPlayed = [NSDate dateWithTimeIntervalSince1970: [level lastPlayed]];
+    NSDateFormatter * formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"dd-MM-yyyy"];
+    [[levelCell subtitleLabel] setText: [formatter stringFromDate:lastPlayed]];
+    [[levelCell subtitleLabel] setFont:font_kc_med(14)];
+    
+    
+    if([level gameType] == GameType_Creative) {
+        [[levelCell thumbnailImage] setImage:[UIImage imageNamed:@"pickaxe"]];
+    } else {
+        [[levelCell thumbnailImage] setImage:[UIImage imageNamed:@"survival"]];
+    }
     
     return levelCell;
 }
