2aa21b53c75698132ac797e7540cb5e480ce571e
CasperCL <casperjeekhof@gmail.com>
2014-01-30 14:09:37 +0100

    Added Defaults, links and keys to important file locations

--- MCSwitcher/Classes/Helper/Constants.h
+++ MCSwitcher/Classes/Helper/Constants.h
@@ -12,5 +12,9 @@
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
 #define font_kc_med(fSize) [UIFont fontWithName:@"KeepCalm-Medium" size:fSize]
+#define DEFAULTS [NSUserDefaults standardUserDefaults]
+#define kMinecraftLoc @"minecraft_loc"
+#define kMinecraftWorldsLoc @"minecraft_worlds_loc"
+#define microPathMCWorlds @"Documents/games/com.mojang/minecraftWorlds/"
 
 #endif
