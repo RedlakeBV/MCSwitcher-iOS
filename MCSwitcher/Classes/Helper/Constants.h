2bed06d6fa73bf059f888df5c25335ece3c237ef
CasperCL <casperjeekhof@gmail.com>
2014-01-31 11:22:41 +0100

    Added new color

--- MCSwitcher/Classes/Helper/Constants.h
+++ MCSwitcher/Classes/Helper/Constants.h
@@ -11,6 +11,9 @@
 
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
+#define green_light 0x33ad38
+
+
 #define font_kc_med(fSize) [UIFont fontWithName:@"KeepCalm-Medium" size:fSize]
 #define DEFAULTS [NSUserDefaults standardUserDefaults]
 #define kMinecraftLoc @"minecraft_loc"
